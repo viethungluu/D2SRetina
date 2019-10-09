51be7e6c8a0f3fae06d36742d4e29f164a1942c0
Lưu Hưng <hunglv@fimo.edu.vn>
2019-10-09 16:58:55 +0700

    print model summary

--- detection/keras_retinanet/bin/train.py
+++ detection/keras_retinanet/bin/train.py
@@ -492,7 +492,7 @@ def main(args=None):
         )
 
     # print model summary
-    # print(model.summary())
+    print(model.summary())
 
     # this lets the generator compute backbone layer shapes using the actual backbone model
     if 'vgg' in args.backbone or 'densenet' in args.backbone:
