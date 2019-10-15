538aab73ea773e024846a33168fef7fc1bedf941
Lưu Hưng <hunglv@fimo.edu.vn>
2019-10-15 16:41:31 +0700

    tsne using l2 distance as metric

--- recognition/visualization.py
+++ recognition/visualization.py
@@ -60,7 +60,7 @@ def main():
 		model.cuda()
 
 	embeddings, labels 	= extract_embeddings(data_loader, model, cuda=cuda)
-	embeddings_tsne 	= TSNE(n_components=2).fit_transform(embeddings)
+	embeddings_tsne 	= TSNE(n_components=2, metric="euclidean").fit_transform(embeddings)
 	plot_embeddings(embeddings_tsne, labels, dataset.num_classes())
 
 if __name__ == '__main__':
