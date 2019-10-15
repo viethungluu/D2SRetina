64e443104533f0ed78bb54dbe9c43f0c1f24b393
Lưu Hưng <hunglv@fimo.edu.vn>
2019-10-15 08:57:42 +0700

    na

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -23,7 +23,6 @@ class CoCoDataset(Dataset):
 		print("Reading dataset from", os.path.join(data_dir, 'annotations', 'D2S_' + set_name + '.json'))
 		self.coco 		= COCO(os.path.join(data_dir, 'annotations', 'D2S_' + set_name + '.json'))
 		self.image_ids 	= self.coco.getImgIds()
-		print(self.image_ids)
 		self._load_classes()
 		self._load_annotations()
 
@@ -87,7 +86,7 @@ class CoCoDataset(Dataset):
 		bbox 	= self.annotations['bboxes'][index]
 		label 	= self.annotations['labels'][index]
 
-		print(imgId, bbox, self.label_to_coco_label(label))
+		print(imgId, bbox, self.coco_label_to_name(self.label_to_coco_label(label)))
 
 		image_info 	= self.coco.loadImgs(imgId)[0]
 		path 		= os.path.join(self.data_dir, 'images', image_info['file_name'])
